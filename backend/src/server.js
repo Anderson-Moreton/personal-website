f1a989ad147b70102208efbf9c9a0ec67de1e5d3
Anderson Moreton <anderson.moreton@gmail.com>
2026-01-31 15:23:30 +0000

    feat: review admin page

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,6 +22,7 @@ app.use('/testimonials', testimonialsRoutes);
 app.use('/testimonials', testimonialsLikesRoutes);
 app.use('/admin', adminRoutes);
 
-app.listen(3000, () =>
-  console.log('Server running on http://localhost:3000')
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server running on port: ${PORT}`)
 );
