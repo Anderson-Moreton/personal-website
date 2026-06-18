83621930d438094e6e37520253370c15f601dc12
Anderson Moreton <anderson.moreton@gmail.com>
2026-06-18 15:06:48 +0100

    feat: add OPA Delivery App repository card

--- frontend/src/app/my-repository/my-repository.component.ts
+++ frontend/src/app/my-repository/my-repository.component.ts
@@ -7,34 +7,30 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './my-repository.component.html',
-  styleUrls: ['./my-repository.component.css']
+  styleUrls: ['./my-repository.component.css'],
 })
 export class MyRepositoryComponent {
-
   sidebarActive = false;
 
   projects = [
     {
-      title: 'Project under development',
+      title: 'OPA Delivery App',
       description:
-        'The project is still in progress. More details will be available soon.',
-      image: 'assets/img/profileRepository.png',
-      link: 'https://github.com/Anderson-Moreton'
+        'OPA Delivery App is a food delivery platform designed to simulate a real-world e-commerce application for restaurants and fast-food businesses. The system includes user authentication, product and category management, shopping cart functionality, order tracking, and an administrative panel for managing orders and updating their status. The project was built using Angular, TypeScript, Node.js, Express, MySQL, and Docker, following a REST API architecture.',
+      image: 'assets/img/opaDeliveryApp.PNG',
+      link: 'https://github.com/Anderson-Moreton/opa-delivery-app',
     },
     {
       title: 'Project under development ',
-      description:
-        'The project is still in progress. More details will be available soon.',
+      description: 'The project is still in progress. More details will be available soon.',
       image: 'assets/img/profileRepository.png',
-      link: 'https://github.com/Anderson-Moreton'
+      link: 'https://github.com/Anderson-Moreton',
     },
     {
       title: 'Project under development ',
-      description:
-        'The project is still in progress. More details will be available soon.',
+      description: 'The project is still in progress. More details will be available soon.',
       image: 'assets/img/profileRepository.png',
-      link: 'https://github.com/Anderson-Moreton'
-    }
+      link: 'https://github.com/Anderson-Moreton',
+    },
   ];
-
 }
